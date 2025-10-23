a73db4f83095dbd972546675aabd66ddd13666b6
Ahmed1010 <Ahmed01020238429@gmail.com>
2025-10-23 15:32:21 +0300

    Add Unit Of Work

---
+++ LibraryManagmentSystem.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using LibraryManagmentSystem.Domain.Entity;
+
+namespace LibraryManagmentSystem.Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>;
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ LibraryManagmentSystem.Infrastructure/Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using LibraryManagmentSystem.Domain.Contracts;
+using LibraryManagmentSystem.Domain.Entity;
+using LibraryManagmentSystem.Infrastructure.Data.Context;
+
+namespace LibraryManagmentSystem.Infrastructure.Data.Repositories
+{
+    public class UnitOfWork(LibraryDbContext dbContext) : IUnitOfWork
+    {
+        private readonly Dictionary<string, object> repositories = [];
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+            var RepoType = typeof(TEntity).Name;
+            return CheckRepository<TEntity, TKey>(dbContext, RepoType);
+        }
+
+        public async Task<int> SaveChangesAsync() => await dbContext.SaveChangesAsync();
+
+        private IGenericRepository<TEntity, TKey> CheckRepository<TEntity, TKey>(LibraryDbContext dbContext, string RepoType) where TEntity : BaseEntity<TKey>
+        {
+            if (repositories.ContainsKey(RepoType))
+            {
+                return (IGenericRepository<TEntity, TKey>)repositories[RepoType];
+            }
+            else
+            {
+                var Repo = new GenericRepository<TEntity, TKey>(dbContext);
+                repositories.Add(RepoType, Repo);
+                return Repo;
+            }
+        }
+    }
+}
