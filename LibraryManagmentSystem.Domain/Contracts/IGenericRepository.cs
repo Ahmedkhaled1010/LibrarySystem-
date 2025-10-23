13908caaa038603b911cc412cc67777c926e2ecc
Ahmed1010 <Ahmed01020238429@gmail.com>
2025-10-23 15:26:38 +0300

    Add GenericRepository

---
+++ LibraryManagmentSystem.Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using LibraryManagmentSystem.Domain.Entity;
+
+namespace LibraryManagmentSystem.Domain.Contracts
+{
+    public interface IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(TKey id);
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ LibraryManagmentSystem.Infrastructure/Data/Repositories/GenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using LibraryManagmentSystem.Domain.Contracts;
+using LibraryManagmentSystem.Domain.Entity;
+using LibraryManagmentSystem.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagmentSystem.Infrastructure.Data.Repositories
+{
+    public class GenericRepository<TEntity, TKey>(LibraryDbContext libraryDbContext) : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        public async Task AddAsync(TEntity entity) => await libraryDbContext.Set<TEntity>().AddAsync(entity);
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync() => await libraryDbContext.Set<TEntity>().ToListAsync();
+
+
+        public async Task<TEntity?> GetByIdAsync(TKey id) => await libraryDbContext.Set<TEntity>().FindAsync(id);
+
+        public void Delete(TEntity entity) => libraryDbContext.Set<TEntity>().Remove(entity);
+
+
+
+        public void Update(TEntity entity) => libraryDbContext.Set<TEntity>().Update(entity);
+
+    }
+}
