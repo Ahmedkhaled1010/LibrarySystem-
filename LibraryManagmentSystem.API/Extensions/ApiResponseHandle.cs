f8f568f8744762ef1e14f065c129bcadcf8d45b1
Ahmed1010 <Ahmed01020238429@gmail.com>
2025-10-23 15:44:03 +0300

    handle ApiValidationError

---
+++ LibraryManagmentSystem.API/Extensions/ApiResponseHandle.cs
@@ -0,0 +1,25 @@
+﻿using LibraryManagmentSystem.Shared.Error;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagmentSystem.API.Extensions
+{
+    public class ApiResponseHandle
+    {
+        public static IActionResult GenerateApiValidationErrorResponse(ActionContext context)
+        {
+            var errors = context.ModelState
+                   .Where(e => e.Value.Errors.Count > 0)
+                   .Select(e => new ValidationError()
+                   {
+                       Field = e.Key,
+                       Errors = e.Value.Errors.Select(e => e.ErrorMessage)
+                   }).ToArray();
+            var toReturn = new ValidationErrorModel()
+            {
+
+                Errors = errors
+            };
+            return new BadRequestObjectResult(toReturn);
+        }
+    }
+}
