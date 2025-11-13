bd182e1698ecf692f74b4a140d51c5485c050490
AhmedKhaledSheen <ahmed.khaled@tildetech.ae>
2025-11-13 13:44:05 +0200

    add Yarp.ReverseProxy

---
+++ LibraryManagmentSystem.ProxyService/Program.cs
@@ -0,0 +1,36 @@
+﻿using Yarp.ReverseProxy;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// إضافة خدمة YARP فقط
+builder.Services.AddReverseProxy()
+    .LoadFromMemory(
+        new[]
+        {
+            new Yarp.ReverseProxy.Configuration.RouteConfig
+            {
+                //route هيظهر للfront
+                RouteId = "Library",
+                //cluster بتاع الservices اللي هيروح عليها
+                ClusterId = "LibraryCluster",
+                Match = new() { Path = "/Library/{**catch-all}" }
+            }
+        },
+        new[]
+        {
+            new Yarp.ReverseProxy.Configuration.ClusterConfig
+            {
+                ClusterId = "LibraryCluster",
+                Destinations = new Dictionary<string, Yarp.ReverseProxy.Configuration.DestinationConfig>
+                {
+                    { "dest1", new() { Address = "https://localhost:7164" } }
+                }
+            }
+        });
+
+var app = builder.Build();
+
+// تفعيل الـ Proxy
+app.MapReverseProxy();
+
+app.Run();
